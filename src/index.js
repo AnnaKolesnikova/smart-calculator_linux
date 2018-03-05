8f73372534cc6c8de2785ede3b6e7b02c8f5a3c9
Anna <ani.kolesnikova@gmail.com>
2018-03-05 17:39:34 +0300

    intermediate solution

--- src/index.js
+++ src/index.js
@@ -1,86 +1,85 @@
 class SmartCalculator {
-  constructor(initialValue) {
-      this.expressionStorage = String(initialValue);
-      this.map = new Map([
-          ['^'], ['*'], ['/'], ['-'], ['+']
-      ]);
-  }
-  add(number) {
-      let add = ' + ' + number;
-      this.expressionStorage += add;
-      return this;
-  }
-  subtract(number) {
-      let sub = ' - ' + number;
-      this.expressionStorage += sub;
-      return this;
-  }
-  multiply(number) {
-      let mult = ' * ' + number;
-      this.expressionStorage += mult;
-      return this;
-  }
-  devide(number) {
-      let dev = ' / ' + number;
-      this.expressionStorage += dev;
-      return this;
-  }
-  pow(number) {
-      let pow = ' ^ ' + number;
-      this.expressionStorage += pow;
-      return this;
-  }
-  valueOf() {
-      let stack = [];
-      let polishString = '';
-      let expressionStorageSplitted = this.expressionStorage.split(' ');
+    constructor(initialValue) {
+        this.expressionStorage = String(initialValue);
+        this.map = new Map([
+            ['**'], ['*'], ['/'], ['-'], ['+']
+        ]);
+    }
+    add(number) {
+        let add = ' + ' + number;
+        this.expressionStorage += add;
+        return this;
+    }
+    subtract(number) {
+        let sub = ' - ' + number;
+        this.expressionStorage += sub;
+        return this;
+    }
+    multiply(number) {
+        let mult = ' * ' + number;
+        this.expressionStorage += mult;
+        return this;
+    }
+    devide(number) {
+        let dev = ' / ' + number;
+        this.expressionStorage += dev;
+        return this;
+    }
+    pow(number) {
+        let pow = ' ** ' + number;
+        this.expressionStorage += pow;
+        return this;
+    }
+    valueOf() {
+        let stack = [];
+        let polishString = [];
+        let expressionStorageSplitted = this.expressionStorage.split(' ');
 
-      for (let i = 0; i < expressionStorageSplitted.length; i++) {
-          let currentSymbol = expressionStorageSplitted[i];
-          stack.push(currentSymbol);
+        for (let i = 0; i < expressionStorageSplitted.length; i++) {
+            let currentSymbol = expressionStorageSplitted[i];
+            stack.push(currentSymbol);
 
-          if (this.map.has(currentSymbol)) {
+            if (this.map.has(currentSymbol)) {
 
-          }
-          else {
-              polishString += stack.pop() + ' ';
+            }
+            else if(currentSymbol){
+                polishString.push(stack.pop());
 
-              let prevOper = stack.pop();
-              if (prevOper != undefined) {
-                  polishString += prevOper + ' ';
-              }
+                let prevOper = stack.pop();
+                if (prevOper != undefined) {
+                    polishString.push(prevOper);
+                }
+            }
+        }
 
-          }
-      }
-      let polishStringSplitted = polishString.split(' ');
+        //let polishStringSplitted = polishString.split(' ');
+        //let result = [];
+        let result = 0;
+        for (let i = 0; i < polishString.length; i++) {
+            let currentSymbol = polishString[i];
+            stack.push(currentSymbol);
 
-      for (let i = 0; i < polishStringSplitted.length; i++) {
-          let currentSymbol = polishStringSplitted[i];
+            if(this.map.has(currentSymbol)){
+                stack.pop();
+                let a = stack.pop();
+                let b = stack.pop();
+                result = b + currentSymbol + a;
+                stack.push(result);
+            }
+          
+            // if (currentSymbol == '') {
+            //     continue;
+            // }
 
-          if (currentSymbol == '') {
-              continue;
-          }
-
-          if (!this.map.has(currentSymbol)) {
-              stack.push(currentSymbol);
-          }
-          else {
-              stack.push(eval(stack.pop() + currentSymbol + stack.pop()));
-          }
-      }
-      return stack[0];
-  }
+            // if (!this.map.has(currentSymbol)) {
+            //     stack.push(currentSymbol);
+            // }
+            // else {
+            //     stack.push(eval(stack.pop() + currentSymbol + stack.pop()));
+            // }
+        }
+        return eval(stack[0]); 
+    }
 }
-module.exports = SmartCalculator;
 
-// module.einumberitialValueports = SmartCalculator;
-
-// class A {
-//   valueOf() {
-//     return true ? 1 : 0;
-//   }
-// }
-
-// conumberst assert = require('assert');
-
-// assert.equal(numberew A(), 1);
+ module.exports = SmartCalculator;
